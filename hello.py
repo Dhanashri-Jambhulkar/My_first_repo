a4032a728936104fbd1276f0edacee8037b9c4f4
Dhanashri-Jambhulkar <dhanashri.j12@gmail.com>
2024-05-30 13:30:29 +0530

    Python code on musical band name generator

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Want to generate your music band name? ")
+ans = input("Enter Y for yes and N for no")
+if(ans=="Y"):
+    name = input("What is the name of the city where you born? ")
+    animal = input("Enter the name of your favourite animal: ")
+    print(f'Name of your musical band is {name} {animal}')
+else:
+    print("Ok, Have a nice day")
